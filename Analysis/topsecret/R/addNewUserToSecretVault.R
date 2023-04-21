2b764ffa730432c88434b7b606f8dd974c829304
Mark Bower <mark.bower@yale.edu>
2023-04-21 12:19:40 -0600

    Changed create_new_user to use id_rsa.pub

--- Analysis/topsecret/R/addNewUserToSecretVault.R
+++ Analysis/topsecret/R/addNewUserToSecretVault.R
@@ -12,6 +12,6 @@ addNewUserToSecretVault <- function( username, vault ) {
   current_users = secret::list_users( vault = vault )
   `%nin%` = Negate(`%in%`)
   if ( username %nin% current_users ) {
-    secret::add_user( email=username, public_key = "~/.ssh/id_rsa_root.pub", vault = vault )
+    secret::add_user( email=username, public_key = "~/.ssh/id_rsa.pub", vault = vault )
   }
 }
